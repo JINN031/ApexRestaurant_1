04c1f4af9ca96a121cc272460494dc173eaf7b70
richa222 <richa.acharya@apexcollege.edu.np>
2020-02-03 09:00:51 +0545

    initial commit

---
+++ ApexRestaurant.Repository/RCustomer/CustomerRepository.cs
@@ -0,0 +1,11 @@
+using ApexRestaurant.Repository.Domain;
+namespace ApexRestaurant.Repository.RCustomer
+{
+    public class CustomerRepository : GenericRepository<Customer>, ICustomerRepository
+    {
+        public CustomerRepository(RestaurantContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+    }
+}
---
+++ rootQuery/Customer.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Customers(
+Id INT PRIMARY KEY IDENTITY(1,1),
+FirstName NVARCHAR(200),
+LastName NVARCHAR(200),
+Address NVARCHAR(500),
+PhoneRes NVARCHAR(50),
+PhoneMob NVARCHAR(50),
+EnrollDate DATETIME,
+IsActive BIT,
+CreatedBy NVARCHAR(200),
+CreatedOn DATETIME,
+UpdatedBy NVARCHAR(200),
+UpdatedOn DATETIME
+);
